95bc73d090a2ada30930ae85de58d883e03c1f68
Florence <ruslanborsuk80@gmail.com>
2023-01-10 15:46:25 +0200

    v.1.1

--- src/main/java/example/game_project/GameController.java
+++ src/main/java/example/game_project/GameController.java
@@ -1,61 +1,53 @@
 package example.game_project;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Box;
-import javafx.scene.shape.Circle;
-
 import java.io.IOException;
-import java.util.Vector;
-
 import static example.game_project.PersonController.*;
+
 
 public class GameController {
     @FXML
-    public Label a;
-    @FXML
-    public Label h;
-    @FXML
-    public Label n;
-    @FXML
-    private ImageView pers1;
+    private Label a;
 
     @FXML
-    private ImageView pers2;
+    private Label a1;
 
+    @FXML
+    private Label h;
+
+    @FXML
+    private Label h1;
+
+    @FXML
+    private Button hitB;
+
+    @FXML
+    private Label n;
+
+    @FXML
+    private Label n1;
+
+
+    String firstName = String.valueOf(n1);
+    int firstDMG = Integer.parseInt(String.valueOf(a1));
+    int firstHealth = Integer.parseInt(String.valueOf(h1));
 
     @FXML
     void initialize() throws IOException {
         n.setText( name1);
         h.setText( healt1);
         a.setText( armore1);
-        }
+        T_INFO firstcard = new T_INFO(firstName, firstDMG, firstHealth);
 
-    public void key(KeyEvent evt) throws IOException{
-        if (evt.getCode() == KeyCode.W){
-        pers1.relocate(0,-5);
-        }
-        if (evt.getCode() == KeyCode.S){
-            pers1.relocate(pers1.getLayoutX()-5, pers1.getLayoutY()-5);
-        }
-        if (evt.getCode() == KeyCode.A){
-            pers1.relocate(pers1.getLayoutX()-5, pers1.getLayoutY()-5);
-        }
-        if (evt.getCode() == KeyCode.D){
-            pers1.relocate(pers1.getLayoutX()-5, pers1.getLayoutY()-5);
-            System.out.println("test");
-        }
-    }
+
+
+        n1.setText(firstcard.getName());
+        h1.setText(String.valueOf(firstcard.getHealthPoint()));
+        a1.setText(String.valueOf(firstcard.getArmore()));
+
+                }
+
 }
 
--- src/main/java/example/game_project/PersonController.java
+++ src/main/java/example/game_project/PersonController.java
@@ -1,11 +1,17 @@
 package example.game_project;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class PersonController {
@@ -14,63 +20,74 @@ public class PersonController {
     static String healt1;
     static String armore1;
 
+    public String extName;
+    public int extDamage;
+    public int extHealth;
+
     @FXML
-    public  TextField arm;
+    public TextField arm;
     @FXML
     private Button crt;
     @FXML
-    public  TextField hp;
+    public TextField hp;
     @FXML
-    public  TextField name;
+    public TextField name;
     @FXML
     private Label error;
+    @FXML
+    private Button str;
+    @FXML
+    private TableView<T_INFO> tableinfo = new TableView<T_INFO>();
+
+    @FXML
+    private TableColumn<T_INFO, Integer> tarm;
+
+    @FXML
+    private TableColumn<T_INFO, Integer> thp;
+
+    @FXML
+    private TableColumn<T_INFO, String> tname;
+
+    ObservableList<T_INFO> list = FXCollections.observableArrayList();
 
     @FXML
     void initialize() {
+        List<Object> chooseList = new ArrayList<>();
 
         crt.setOnAction(actionEvent1 -> {
-           name1 = name.getText().trim();
-           healt1 = hp.getText().trim();
-           armore1 = arm.getText().trim();
-            if (!name1.isEmpty() && !healt1.isEmpty() && !armore1.isEmpty()) {
+            name1 = name.getText().trim();
+            healt1 = hp.getText().trim();
+            armore1 = arm.getText().trim();
 
-                crt.getScene().getWindow().hide();
-                FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(getClass().getResource("game.fxml"));
-                try {
-                    loader.load();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Parent root = loader.getRoot();
-                Stage stage = new Stage();
-                stage.setScene(new Scene(root));
-                stage.setTitle("Game");
-                stage.show();
-            } else {
-                        error.setText("Заповніть усі поля");
-                    }
-            if (!(healt1 == String.valueOf(100)) && !(armore1 == String.valueOf(100))) {
-                crt.getScene().getWindow().hide();
-                FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(getClass().getResource("game.fxml"));
-                try {
-                    loader.load();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Parent root = loader.getRoot();
-                Stage stage = new Stage();
-                stage.setScene(new Scene(root));
-                stage.setTitle("Game");
-                stage.show();
-            } else {
-                error.setText("HP чи ARMORE не може бути більше за 100");
-                    }
+
+            tname.setCellValueFactory(new PropertyValueFactory<T_INFO, String>("Name"));
+            thp.setCellValueFactory(new PropertyValueFactory<T_INFO, Integer>("HealthPoint"));
+            tarm.setCellValueFactory(new PropertyValueFactory<T_INFO, Integer>("Armore"));
+
+
+            String names = name.getText().trim();
+            int health = Integer.parseInt(hp.getText().trim());
+            int armore = Integer.parseInt(arm.getText().trim());
+            tableinfo.setItems(list);
+            list.add(new T_INFO(names, health, armore));
 
     });
+        str.setOnAction(actionEvent1 -> {
+            str.getScene().getWindow().hide();
 
-        }
-
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("fight.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Fight!");
+            stage.show();
+        });
     }
+}
 
